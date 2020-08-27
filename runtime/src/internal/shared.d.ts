e4ddd1042f90b02058c7399e53823cd8b9232546
Conduitry <git@chor.date>
2020-08-27 15:39:38 -0400

    [build site]

--- runtime/src/internal/shared.d.ts
+++
@@ -1,5 +0,0 @@
-export const CONTEXT_KEY: unknown;
-
-export type Params = Record<string, string>;
-export type Query = Record<string, string | string[]>;
-export type Preload = (props: { params: Params, query: Query }) => Promise<any>;
